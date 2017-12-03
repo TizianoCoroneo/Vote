4899f606bf747f7158f77543653f91a1a9291540
Tiziano Coroneo <tizianocoroneo@me.com>
2017-12-03 17:04:12 +0100

    Added the first question tableview

---
+++ Vote/Utilities/Color Utilities.swift
@@ -0,0 +1,17 @@
+//
+//  Color Utilities.swift
+//  Vote
+//
+//  Created by Tiziano Coroneo on 03/12/2017.
+//  Copyright © 2017 Tiziano Coroneo. All rights reserved.
+//
+
+import UIKit
+import DynamicColor
+
+extension UIColor {
+    
+    static var voteLightGrey: UIColor { return UIColor(hexString: "#eeeeee") }
+    static var voteDarkGrey: UIColor { return UIColor(hexString: "#4a4a4a") }
+    
+}
---
+++ Vote/Utilities/StyleGlobals.swift
@@ -0,0 +1,15 @@
+//
+//  StyleGlobals.swift
+//  Vote
+//
+//  Created by Tiziano Coroneo on 03/12/2017.
+//  Copyright © 2017 Tiziano Coroneo. All rights reserved.
+//
+
+import UIKit
+
+class Style {
+    
+    static let cornerRadius: CGFloat = 10
+    
+}
